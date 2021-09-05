3b3ec7d3cb6f5366d10743c62b6ff3a113e874bf
Nico Gevers <me@gevious.com>
2021-09-05 15:51:02 -0400

    Capture names of players

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Monopoly clone, to help visually impaired keep track of resources
 
 # Roadmap 
 - [x] Infinite loop asking for dice roll
-- [ ] Entering num players, and record name of each player
-- [ ] Each player has a turn. Dice number moves player to correct position
+- [x] Entering num players, and record name of each player
 - [ ] Show summary of all players at start of each turn (money, position, assets)
+- [ ] Each player has a turn. Dice number moves player to correct position
 - [ ] Game displays position player lands on
 - [ ] Game displays options a player has on each position
 - [ ] Player can receive money from bank
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::io::{Write};
+use std::convert::TryInto;
 
 /// Get the dice roll from the user
 // This method gets a user input, validates its a number, and the number is within range
@@ -20,23 +21,91 @@ fn get_dice_roll(user_input: String) -> Result<i32, ()> {
     }
 }
 
-fn main() {
+/// Get the number of players
+fn get_player_num(user_input: String) -> Result<i32, ()> {
+    // Convert to number
+    let player_num = match user_input.parse::<i32>() {
+        Ok(d) => d,
+        Err(_e) => {
+            return Err(());
+        }
+    };
+    match player_num {
+        2..=8 => Ok(player_num),
+        _ => {
+            Err(())
+        }
+    }
+}
+
+/// Capture the name of a player
+fn capture_names(player_num: i32) -> Vec<String> {
+    let mut players = Vec::<String>::with_capacity(player_num.try_into().unwrap());
+    for mut p in 1..(player_num+1) {
+        print!("Enter name for Player {}: ", p);
+        let _= io::stdout().flush();
+        let mut user_input = String::new();
+        match io::stdin().read_line(&mut user_input) {
+            Ok(name) => {
+                let mut player_name = name.to_string();
+                player_name.pop(); // Remove newline
+                players.push(player_name);
+            },
+            Err(_) => {
+                println!("Invalid name");
+                p = p - 1; // repeat player
+            }
+        };
+    }
+    players
+}
+
+/// Capture the amount of players
+fn capture_player_num() -> i32 {
+    loop {
+        print!("Enter amount of players: ");
+        let _= io::stdout().flush();
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("Did not enter a valid number");
+        user_input.pop(); // Remove newline
+
+        match get_player_num(user_input) {
+            Ok(amt) => {
+                return amt;
+            },
+            Err(_) => {
+                println!("Enter a valid number between 1 and 8");
+            }
+        };
+    }
+}
+
+/// Capture the roll of the dice
+fn capture_dice_roll() -> i32 {
     loop {
         print!("Enter dice roll: ");
         let _= io::stdout().flush();
         let mut user_input = String::new();
-        io::stdin().read_line(&mut user_input).expect("Did not enter a correct string");
+        io::stdin().read_line(&mut user_input).expect("Did not enter a valid number");
         user_input.pop(); // Remove newline
 
-        let dice_roll = match get_dice_roll(user_input) {
-            Ok(d) => d,
-            Err(s) => {
+        match get_dice_roll(user_input) {
+            Ok(roll) => {
+                return roll;
+            },
+            Err(_) => {
                 println!("Enter a number between 2 and 12");
                 continue;
             }
         };
-        println!("You rolled {}", dice_roll);
     }
+}
+
+fn main() {
+    let player_num = capture_player_num();
+    let players = capture_names(player_num);
+    let dice_roll = capture_dice_roll();
+    println!("You rolled {}", dice_roll);
 }
 
 #[cfg(test)]
@@ -52,8 +121,16 @@ mod tests {
     #[test]
     fn test_dice_number_in_range() {
         assert_eq!(get_dice_roll("1".to_string()), Err(()));
-        assert_eq!(get_dice_roll("2".to_string()), Ok((2)));
-        assert_eq!(get_dice_roll("12".to_string()), Ok((12)));
+        assert_eq!(get_dice_roll("2".to_string()), Ok(2));
+        assert_eq!(get_dice_roll("12".to_string()), Ok(12));
         assert_eq!(get_dice_roll("13".to_string()), Err(()));
+    }
+
+    #[test]
+    fn test_player_num_in_range() {
+        assert_eq!(get_player_num("1".to_string()), Err(()));
+        assert_eq!(get_player_num("2".to_string()), Ok(2));
+        assert_eq!(get_player_num("8".to_string()), Ok(8));
+        assert_eq!(get_player_num("9".to_string()), Err(()));
     }
 }

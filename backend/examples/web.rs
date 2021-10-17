989665bacd0bee78963924203c20c234203d0b06
Nico Gevers <me@gevious.com>
2021-10-17 09:44:53 -0400

    Changed to rocket framework. Working with CORS

--- backend/examples/web.rs
+++ backend/examples/web.rs
@@ -1,6 +1,8 @@
-use actix_cors::Cors;
-use actix_web::{web, get, post, App, HttpResponse, HttpServer};
-use actix_web::middleware::Logger;
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+use rocket::State;
+use rocket::fairing::Fairing;
+use rocket::http::{Method, ContentType, Status, Header};
 use serde::{Deserialize, Serialize};
 use log::{info};
 
@@ -8,8 +10,7 @@ use std::sync::Mutex;
 use monopoly::{game};
 
 struct AppState {
-    num: Mutex<u32>,
-//    game: Mutex<game::Game>
+    game: Mutex<game::Game>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -19,73 +20,47 @@ struct DiceRoll {
 }
 
 #[get("/ping")]
-async fn ping() -> String {
+fn ping() -> String {
     format!("pong")
 }
 
-#[post("/roll-dice")]
+#[post("/roll-dice", format="application/json", data="<input>")]
 /// Roll the dice in an established game.
 // Expects a request like: 
 // curl -X POST -H "Content-type: application/json" \
 //      -d '{"dice_roll": {"dice1": 3, "dice2": 2}}' \
 //      http://127.0.0.1:8000/roll-dice
-async fn roll_dice(app_state: web::Data<AppState>,
-                   dice_roll: web::Json<DiceRoll>) -> HttpResponse {
-    info!("Got {:?}", dice_roll);
+fn roll_dice(input: String,
+                   app_state: State<AppState>) -> String {
+    info!("Got {:?}", input);
+    let dice_roll: DiceRoll = serde_json::from_str(&input).unwrap();
 
-//    let dice = game::Dice::new(dice_roll.dice1, dice_roll.dice2);
-//    let g = app_state.game.lock().unwrap();
-//    info!("Active player: {:?}", g.active_player());
-//    g.go(dice);
-//    info!("Active player: {:?}", g.active_player());
-//
-    let mut n = app_state.num.lock().unwrap();
-    info!("num b4: {}", n);
-    *n += 1;
-    info!("num aftr: {}", n);
-    // TODO: pass back all game information
-    let dice = DiceRoll { dice1: *n, dice2: 2};
-    let response = format!("{}",
-                           serde_json::to_string(&dice).unwrap());
+    let dice = game::Dice::new(dice_roll.dice1, dice_roll.dice2);
+    let g = app_state.game.lock().unwrap();
+    info!("Active player: {:?}", g.active_player());
+    g.go(dice);
+    info!("Active player: {:?}", g.active_player());
 
-    HttpResponse::Ok()
-        .content_type("application/json; charset=utf-8")
-        .body(response)
+    let dice = DiceRoll { dice1: 1, dice2: 2};
+    serde_json::to_string(&dice).unwrap()
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
 
-    let app_data = web::Data::new(AppState { 
-        num: Mutex::new(0),
-//        game: Mutex::new(game::init(vec![
-//                "Hannah".to_string(),
-//                "Daniel".to_string(),
-//                "Daddy".to_string(),
-//        ]))
-    });
-    HttpServer::new(move || {
-        // TODO: Only for local testing
-        let cors = Cors::default()
-            .allowed_origin("http://localhost:3000") // local react app
-            .allow_any_method()
-            .allow_any_header();
-        App::new()
-            .wrap(Logger::default())
-            .wrap(cors)
-//            .app_data(app_data)
-            .app_data(app_data.clone())
-//            .app_data(web::Data::new(AppState {
-//                num: Mutex::new(0),
-//                game: Mutex::new(game::init(
-//                        vec!["Hannah".to_string(), "Daniel".to_string(),
-//                             "Daddy".to_string()]))
-//            }))
-            .service(ping)
-            .service(roll_dice)
-    })
-    .bind(("localhost", 8000))?
-    .run()
-    .await
+fn main() {
+    let app_state = AppState { 
+        game: Mutex::new(game::init(vec![
+                "Hannah".to_string(),
+                "Daniel".to_string(),
+                "Daddy".to_string(),
+        ]))
+    };
+
+    let default = rocket_cors::CorsOptions::default().to_cors()
+        .expect("error creating CORS fairing");
+
+    rocket::ignite()
+        .manage(app_state)
+        .attach(default)
+        .mount("/", routes![ping, roll_dice])
+        .launch();
 }
